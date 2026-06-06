965df4060a5c902cd46d2a942ca8eedb75219595
DESKTOP-JHGUF0G\Админ <arsenn1411@gmail.com>
2026-06-06 16:29:59 +0100

    Done

--- linear_regression.py
+++ linear_regression.py
@@ -2,8 +2,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-a =1.8
-b= 32
+a =0.5
+b= 100
+
+def derivative_a(n,x,y,a,b):
+    t = 0
+    for i in range(n):
+        t += x[i]*(y[i] - formula(x,i,a,b))
+    return (-2/n)*t
+
+def derivative_b(n,x,y,a,b):
+    t = 0
+    for i in range(n):
+        t += (y[i] - formula(x,i,a,b))
+    return (-2/n)*t
 
 def formula(x,i,a,b):
     return a*x[i] + b 
@@ -14,13 +26,37 @@ def sum_of_points(n,x,y,a,b):
         t += (y[i] - formula(x,i,a,b))**2
     return t
 
-x = [-40, -30, -20, -10, -5, 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
-y = [-40, -22, -4, 14, 23, 32, 41, 50, 59, 68, 77, 86, 95, 104, 113, 122, 131, 140, 149, 158, 167, 176, 185, 194, 203, 212]
+x = [-10, -8, -6, -4, -2, 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+y = [-35, 3, -22, 15, -8, 25, -2, 34, 10, 45, 18, 58, 30, 72, 40, 85]
 
 n = len(x)
 E = (1/n) * sum_of_points(n,x,y,a,b)
 print(E)
+learning_rate = 0.0001
+epochs = 50000
 
+for epoch in range(epochs):
+    da = derivative_a(n, x, y, a, b)
+    db = derivative_b(n, x, y, a, b)
 
-plt.scatter(x, y)
+    a = a - learning_rate * da
+    b = b - learning_rate * db
+
+    E = (1/n) * sum_of_points(n, x, y, a, b)
+
+    if epoch % 1000 == 0:
+        print(epoch, E, a, b)
+
+y_pred = []
+
+for i in range(n):
+    y_pred.append(formula(x, i, a, b))
+
+print("Final E:", E)
+print("Final a:", a)
+print("Final b:", b)
+
+plt.scatter(x, y, label="Real data")
+plt.plot(x, y_pred, label="Learned line")
+plt.legend()
 plt.show()
